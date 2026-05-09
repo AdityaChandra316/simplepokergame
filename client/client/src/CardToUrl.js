6d4c09afec82dd74f819e6f7de8f4e41a4ddc394
Swati Rawat <swatirawat@wirelessprv-10-195-98-234.near.illinois.edu>
2026-05-08 22:48:18 -0500

    Remove nested client folder

--- client/client/src/CardToUrl.js
+++
@@ -1,7 +0,0 @@
-function CardToUrl(card) {
-  const suit = card % 4;
-  const rank = (card - suit) / 4;
-  return `/cards/row-${suit + 1}-column-${rank == 12 ? 1 : rank + 2}.png`;
-}
-
-export default CardToUrl;
